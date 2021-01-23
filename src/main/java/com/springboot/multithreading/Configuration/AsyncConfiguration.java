355bf6abeb233a9b990433471ad7c505756e1afe
evan999 <emeshberge@gmail.com>
2021-01-23 18:19:08 -0500

    Fix task Executor bug - initialize ThreadPoolTaskExector, not ThreadPoolExecutor. Fix name.

--- src/main/java/com/springboot/multithreading/Configuration/AsyncConfiguration.java
+++ src/main/java/com/springboot/multithreading/Configuration/AsyncConfiguration.java
@@ -4,13 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.websocket.RemoteEndpoint;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
-@EnableSync
+@EnableAsync
 public class AsyncConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AsyncConfiguration.class);
@@ -18,9 +20,9 @@ public class AsyncConfiguration {
     @Bean(name = "taskExecutor")
     public Executor taskExecutor() {
         LOGGER.debug("Creating Async Task Executor");
-        final ThreadPoolExecutor executor = new ThreadPoolExecutor();
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
-        executor.setMaximumPoolSize(2);
+        executor.setMaxPoolSize(2);
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("CarThread-");
         executor.initialize();
